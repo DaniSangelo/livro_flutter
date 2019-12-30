5defac2e467651a0a29dd4edd7cecdf3053137e7
evertonfoz <evertoncoimbra@gmail.com>
2019-12-30 12:40:56 -0300

    Iniciado Capítulo 7 - Custom Drawer

---
+++ capitulo_07_custom_drawer/lib/drawer/widgets/drawer_controller_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'appbar_widget.dart';
+
+class DrawerControllerWidget extends StatelessWidget {
+  final GlobalKey<DrawerControllerState> _drawerKey =
+      GlobalKey<DrawerControllerState>();
+
+  final AppBarWidget appBar;
+  final Widget body;
+  final Drawer drawer;
+
+  DrawerControllerWidget(
+      {@required this.appBar, @required this.body, @required this.drawer});
+
+  void openDrawer() {
+    _drawerKey.currentState?.open();
+  }
+
+  void drawerCallback(bool status) {
+    appBar.callbackFunction(status);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Stack(
+          children: [
+            // body
+            Positioned(
+              top: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: AppBar(
+                automaticallyImplyLeading: appBar.automaticallyImplyLeading,
+                actions: <Widget>[
+                  GestureDetector(
+                      child: Icon(Icons.menu),
+                      onTap: () {
+                        openDrawer();
+                      }),
+                ],
+                title: Text(
+                  "Jogo da Forca",
+                ),
+                centerTitle: true,
+              ),
+            ),
+
+            body,
+
+            DrawerController(
+              edgeDragWidth: 100,
+              key: _drawerKey,
+              alignment: DrawerAlignment.end,
+              child: drawer,
+              drawerCallback: (status) => drawerCallback(status),
+            ),
+          ],
+        ),
+      );
+}
---
+++ capitulo_07_custom_drawer/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'functions/device_functions.dart' as DeviceFunctions;
+import 'routes/splash_screen_route.dart';
+import 'scopedmodel/drawer_scoped_model.dart';
+
+void main() => runApp(ForcaApp());
+
+class ForcaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<DrawerScopedModel>(
+      model: DrawerScopedModel(),
+      child: MaterialApp(
+        title: 'Forca da UTFPR',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.green,
+        ),
+        home: ForcaHomePage(),
+      ),
+    );
+  }
+}
+
+class ForcaHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    DeviceFunctions.Dimensoes.deviceHeight = MediaQuery.of(context).size.height;
+    DeviceFunctions.Dimensoes.deviceWidth = MediaQuery.of(context).size.width;
+    DeviceFunctions.Dimensoes.shortestSide =
+        MediaQuery.of(context).size.shortestSide;
+    DeviceFunctions.Dimensoes.safeAreaTop = MediaQuery.of(context).padding.top;
+    DeviceFunctions.Dimensoes.safeAreaBottom =
+        MediaQuery.of(context).padding.bottom;
+
+    return Scaffold(
+      body: SplashScreenRoute(),
+    );
+  }
+}
