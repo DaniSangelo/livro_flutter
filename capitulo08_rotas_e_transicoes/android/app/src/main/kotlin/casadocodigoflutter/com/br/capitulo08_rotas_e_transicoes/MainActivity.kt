28e1a6acb2e8073d046dcb8cc51f6d0ed0736279
evertonfoz <evertoncoimbra@gmail.com>
2020-01-19 08:24:19 -0300

    Iniciado Capítulo 8

---
+++ capitulo08_rotas_e_transicoes/android/app/src/main/kotlin/casadocodigoflutter/com/br/capitulo08_rotas_e_transicoes/MainActivity.kt
@@ -0,0 +1,12 @@
+package casadocodigoflutter.com.br.capitulo08_rotas_e_transicoes
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
